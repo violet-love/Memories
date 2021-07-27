a806082cc4b1f705069b8f2d2f8a77efa3f34198
Violet Love <58837492+violet-love@users.noreply.github.com>
2021-07-27 03:06:35 -0700

    connected to database

--- server/index.js
+++ server/index.js
@@ -9,4 +9,17 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL =
+  "mongodb+srv://javascriptmastery:0117@cluster0.xkqrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
+
 //https://www.mongodb.com/cloud/atlas
