16fa91dd57db1700e9cd306b129cbe69893e142e
Violet Love <58837492+violet-love@users.noreply.github.com>
2021-07-27 17:46:00 -0700

    created router

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
